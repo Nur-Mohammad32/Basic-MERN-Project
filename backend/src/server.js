1c06f9de8c67ba4a3b27e9cd78b5d63867f6d61f
Nur Mohammad <nurmohammad151801@gmail.com>
2026-05-24 17:11:25 +0600

    .

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,18 +2,21 @@ import express from "express";
 import noteRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+
+//this is necessary for production level application
 const startServer = async () => {
   try {
-    await connectDB();
-
-    app.listen(PORT, () => {
-      console.log("Server started on port :", PORT);
+    await connectDB().then(() => {
+      app.listen(PORT, () => {
+        console.log("Server started on port :", PORT);
+      });
     });
   } catch (error) {
     process.exit(1);
@@ -23,13 +26,14 @@ const startServer = async () => {
 startServer();
 
 //middleware
-app.use(express.json());//this method will parse JSON bodies:req.body ...basically it helps  us to access the req.body
+app.use(express.json()); //this method will parse JSON bodies:req.body ...basically it helps  us to access the req.body
+app.use(rateLimiter);
 
 //our simple custom middleware
-// app.use((req, res, next) => { 
+// app.use((req, res, next) => {
 //   console.log(`Request method id ${req.method} & req URL is ${req.url}`);
 //   next();
-  
+
 // })
 
 app.use("/api/notes", noteRoutes);
